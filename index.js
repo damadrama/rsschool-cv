d1e10703e37b54683c76b6e4e5f38b51e90a1555
github-damadrama <department3578@gmail.com>
2021-09-07 00:50:34 +0300

    feat: add changes to CV

---
+++ index.js
@@ -0,0 +1,13 @@
+function filteredArray(numbers, value) {
+  const filteredNumbers = [];
+  numbers.forEach(function (element) {
+    if (element > value) {
+      filteredNumbers.push(element);
+    }
+  });
+  return filteredNumbers;
+}
+console.log(filteredArray([1, 5, 3, 14, 5], 5)),
+console.log(filteredArray([1, 2, 6, 8, 5], 3));
+console.log(filteredArray([11, 12, 3, 4, 5], 13));
+console.log(filteredArray([9, 22, 7, 4, 15], 10));
