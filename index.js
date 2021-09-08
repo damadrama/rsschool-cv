542c2f4ddc31ba88f0a1a28d104081507a0eb442
damadrama <department3578@gmail.com>
2021-09-08 22:21:48 +0300

    refactor: add css folder

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ console.log(filteredArray([1, 5, 3, 14, 5], 5)),
 console.log(filteredArray([1, 2, 6, 8, 5], 3));
 console.log(filteredArray([11, 12, 3, 4, 5], 13));
 console.log(filteredArray([9, 22, 7, 4, 15], 10));
+
+// *
+// !
+// ?
+// TODO
